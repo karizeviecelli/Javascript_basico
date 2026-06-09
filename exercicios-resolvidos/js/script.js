aca98cfaa56adc2b6d31d8065b0631b11ecd70d3
Sara Evellin Duarte <saraevyn@outlook.com>
2026-06-09 16:41:22 -0300

    Update script.js

--- exercicios-resolvidos/js/script.js
+++ exercicios-resolvidos/js/script.js
@@ -52,3 +52,18 @@ switch (letra) {
     default:
         console.log(`"${entrada}" é uma consoante.`);
 }
+
+// 5
+let nota1 = 8.5;
+let nota2 = 7.5;
+let media = (nota1 + nota2) / 2;
+
+console.log(`Média do aluno: ${media}`);
+
+if (media === 10) {
+    console.log("Aprovado com Distinção");
+} else if (media >= 7) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
